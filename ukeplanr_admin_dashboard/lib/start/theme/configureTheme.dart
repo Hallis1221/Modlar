d9eaa047e141c9acf1f3e5732eb8c07e0fb2ae33
Hallis - home main <halvorvivelstad@gmail.com>
2021-06-23 21:24:10 +0200

    :x: Remove flutter config

--- ukeplanr_admin_dashboard/lib/start/theme/configureTheme.dart
+++ ukeplanr_admin_dashboard/lib/start/theme/configureTheme.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
-import 'package:logger_flutter/logger_flutter.dart';
 import 'package:ukeplanr_admin_dashboard/config/logs/logger.dart';
 import 'package:ukeplanr_admin_dashboard/config/theme/themes.dart';
 import 'package:ukeplanr_admin_dashboard/logic/logs/logService.dart';
@@ -13,6 +12,6 @@ void configureThemes() {
       defaultTheme: Themes().getDefaultTheme,
     ),
   );
-  final Function log = GetIt.instance.get<LogsConfig>().logger.log;
+  final Function log = GetIt.instance.get<LogsConfig>().logger!.log;
   log(Level.info, "Configured Themes!");
 }

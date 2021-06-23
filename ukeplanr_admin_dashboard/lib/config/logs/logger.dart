822ed0a13aaf6c4f55aebbe4194040d1f213a6a8
Hallis - home main <halvorvivelstad@gmail.com>
2021-06-23 21:11:08 +0200

    :paperclip: Rename LoggerConfig to Logs

--- ukeplanr_admin_dashboard/lib/config/logs/logger.dart
+++ ukeplanr_admin_dashboard/lib/config/logs/logger.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 
-class LoggerConfig {
+class Logs {
   final Logger _logger = Logger(
     printer: PrettyPrinter(
       methodCount: 3,

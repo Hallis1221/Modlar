0ed1cbf51ca4bfb31d065c1852c5c310b1a4818c
Hallis - home main <halvorvivelstad@gmail.com>
2021-06-23 21:50:51 +0200

    :newspaper: Rename LogsConfig to Logs

--- ukeplanr_admin_dashboard/lib/logic/logs/logService.dart
+++ ukeplanr_admin_dashboard/lib/logic/logs/logService.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 
-class LogsConfig {
+class Logs {
   final Logger? logger;
 
   Logger? get getLogger => logger;
 
-  LogsConfig({
+  Logs({
     @required this.logger,
   });
 }

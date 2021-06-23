f363cf0050e50ff30a295f965bd06fce858bf8c0
Hallis - home main <halvorvivelstad@gmail.com>
2021-06-23 21:23:45 +0200

    :x: Remove flutter config

--- ukeplanr_admin_dashboard/lib/start/logs/configerLogger.dart
+++ ukeplanr_admin_dashboard/lib/start/logs/configerLogger.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
-import 'package:logger_flutter/logger_flutter.dart';
 import 'package:ukeplanr_admin_dashboard/config/logs/logger.dart';
 import 'package:ukeplanr_admin_dashboard/logic/logs/logService.dart';
 
 void configureLogger() {
   // Init the flutter logger package to be able to show logs on screen
-  LogConsole.init();
   // Set logger level based on log level
   Logger.level = Logs().getLogLevel;
   GetIt.instance.registerSingleton<LogsConfig>(
@@ -15,7 +13,7 @@ void configureLogger() {
       logger: Logs().getLogger,
     ),
   );
-  final Function log = GetIt.instance.get<LogsConfig>().logger.log;
+  final Function log = GetIt.instance.get<LogsConfig>().logger!.log;
   FlutterError.onError = (FlutterErrorDetails error) => log(
         Level.error,
         """

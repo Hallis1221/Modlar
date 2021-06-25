bfab03a1e73cd1e1ed239a32a8856a6a2ccf0393
Hallis - home main <halvorvivelstad@gmail.com>
2021-06-25 23:48:54 +0200

    :baseball: Try catch

--- ukeplanr_admin_dashboard/lib/start/logs/configerLogger.dart
+++ ukeplanr_admin_dashboard/lib/start/logs/configerLogger.dart
@@ -8,19 +8,24 @@ void configureLogger() {
   // Init the flutter logger package to be able to show logs on screen
   // Set logger level based on log level
   Logger.level = LogsConfig().getLogLevel;
-  GetIt.instance.registerSingleton<Logs>(
-    Logs(
-      logger: LogsConfig().getLogger,
-    ),
-  );
-  final Function log = GetIt.instance.get<Logs>().logger!.log;
-  FlutterError.onError = (FlutterErrorDetails error) => log(
+  try {
+    GetIt.instance.registerSingleton<Logs>(
+      Logs(
+        logger: LogsConfig().getLogger,
+      ),
+    );
+  } catch (error) {
+    GetIt.instance.get<Logs>().logger!.log(Level.warning,
+        "Encountered a error while registering logger. The following information was given: $error.");
+  }
+  final Function? log = GetIt.instance.get<Logs>().logger!.log;
+  FlutterError.onError = (FlutterErrorDetails? error) => log!(
         Level.error,
         """
-      Encountered a ${error.library} error. 
+      Encountered a ${error!.library} error. 
       The context was ${error.context} 
       The following information was given: ${error.exception}. 
       """,
       );
-  log(Level.info, "Configured Logs!");
+  log!(Level.info, "Configured Logs!");
 }

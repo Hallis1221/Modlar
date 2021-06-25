8d009cb6d76a6dd0a07fc0f430b41131baaa1213
Hallis - home main <halvorvivelstad@gmail.com>
2021-06-26 00:00:06 +0200

    :a: Remove future

--- ukeplanr_admin_dashboard/lib/start/configureApp.dart
+++ ukeplanr_admin_dashboard/lib/start/configureApp.dart
@@ -2,8 +2,11 @@ import 'package:flutter/widgets.dart';
 import 'package:ukeplanr_admin_dashboard/start/logs/configerLogger.dart';
 import 'package:ukeplanr_admin_dashboard/start/theme/configureTheme.dart';
 
-Future<void> configureApp() async {
-  WidgetsFlutterBinding.ensureInitialized();
+configureApp() {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+  } catch (e) {}
+
   configureLogger();
   configureThemes();
 }

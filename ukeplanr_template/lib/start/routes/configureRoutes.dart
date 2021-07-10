eee736b26f184439a302b2bb8d368f6e0cc8b5ba
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-10 15:53:40 +0200

    :panda_face: Create configureRoutes.dart

---
+++ ukeplanr_template/lib/start/routes/configureRoutes.dart
@@ -0,0 +1,28 @@
+import 'package:ukeplanr_template/config/routes/routes.dart';
+
+import 'package:ukeplanr_template/logic/logs/logService.dart';
+import 'package:ukeplanr_template/logic/routes/routes.dart';
+
+import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
+
+void configureRoutes() {
+  try {
+    GetIt.instance.registerSingleton<RoutesService>(
+      RoutesService(
+        routes: RoutesConfig().getRoutes,
+      ),
+    );
+    try {
+      final Function? log = GetIt.instance.get<LogsService>().logger!.log;
+      log!(Level.info, "Configured Routes!");
+    } catch (e) {}
+  } catch (e) {
+    try {
+      final Function? log = GetIt.instance.get<LogsService>().logger!.log;
+      log!(Level.error, """
+          Failed to configure Routes! The following information was given: $e
+          """);
+    } catch (e) {}
+  }
+}

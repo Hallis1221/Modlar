a653e18289ba776fa2bebbbc64d8aee3e95c55dd
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-10 16:09:22 +0200

    :100: Implement find widgets

--- ukeplanr_template/lib/logic/routes/mapping/findWidget.dart
+++ ukeplanr_template/lib/logic/routes/mapping/findWidget.dart
@@ -1,7 +1,14 @@
+import 'package:ukeplanr_template/components/temp/errors/404.dart';
+import 'package:ukeplanr_template/logic/routes/routes.dart';
+
 import 'package:flutter/widgets.dart';
+import 'package:get_it/get_it.dart';
 
 Widget findWidgetFromRoute({
   @required RouteSettings? settings,
 }) {
-  throw "";
+  Widget? nullableWidget =
+      GetIt.instance<RoutesService>().getRoutes![settings!.name];
+  Widget widget = nullableWidget != null ? nullableWidget : PageNotFound();
+  return widget;
 }

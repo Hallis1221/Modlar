127203d28683fa632d9cb25ba0fec6f567bec087
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-10 16:28:34 +0200

    :100: Extend navigation observer

--- ukeplanr_template/lib/logic/navigation/observers/navigationWatcher.dart
+++ ukeplanr_template/lib/logic/navigation/observers/navigationWatcher.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 
-class NavigationWatcher extends NavigatorObserver {}
+class NavigationWatcher extends NavigatorObserver {
+  // todo implement custom tracking feature for pages
+
+  @override
+  void didReplace({Route? newRoute, Route? oldRoute}) {
+    print("ok so something replaced");
+  }
+}

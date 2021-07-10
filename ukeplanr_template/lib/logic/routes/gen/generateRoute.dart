738030e7104984165fd6bd8d29d148a386820064
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-10 15:47:16 +0200

    :de: Define type for settings in generateRoute

--- ukeplanr_template/lib/logic/routes/gen/generateRoute.dart
+++ ukeplanr_template/lib/logic/routes/gen/generateRoute.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ukeplanr_template/logic/routes/mapping/findWidget.dart';
 
-Route generateRoute(settings) {
+Route generateRoute(RouteSettings settings) {
   return MaterialPageRoute(
     builder: (context) => findWidgetFromRoute(settings: settings),
   );

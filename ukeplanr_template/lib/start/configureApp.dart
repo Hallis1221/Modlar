9790e684a2ed0adca39e75b416111a161d8b0a57
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-10 14:50:40 +0200

    :tram: Rename initialization to configureinitialization

--- ukeplanr_template/lib/start/configureApp.dart
+++ ukeplanr_template/lib/start/configureApp.dart
@@ -1,4 +1,4 @@
-import 'package:ukeplanr_template/start/initialization/initialization.dart';
+import 'package:ukeplanr_template/start/widgets/initialization.dart';
 import 'package:ukeplanr_template/start/logs/configerLogger.dart';
 import 'package:ukeplanr_template/start/theme/configureTheme.dart';
 

a6bd4681ad14b23b45ba5f368aecd06e76c133d1
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-12 16:13:49 +0200

    :chart: Check if route was registered right before

--- ukeplanr_template/lib/logic/navigation/gen/generateRoute.dart
+++ ukeplanr_template/lib/logic/navigation/gen/generateRoute.dart
@@ -7,9 +7,19 @@ import 'package:flutter/material.dart';
 
 Route generateRoute(RouteSettings settings) {
   final Function? log = GetIt.instance.get<LogsService>().logger!.log;
+  final NavigationWatcher navigationWatcher =
+      GetIt.instance<NavigationWatcher>();
+  final List<Map<String?, RouteSettings>> routeHistory =
+      navigationWatcher.getRouteHistory;
 
   try {
-    GetIt.instance<NavigationWatcher>().registerRoute(settings);
+    if (routeHistory.length == 0) {
+      navigationWatcher.registerRoute(settings);
+    } else {
+      // !! This might cause issues when passing arguments
+      if (routeHistory[routeHistory.length - 1].keys.first != settings.name)
+        navigationWatcher.registerRoute(settings);
+    }
   } catch (e) {
     log!("""
         Tried to register ${settings.name} inside NavigationWatcher but got the following error: $e 

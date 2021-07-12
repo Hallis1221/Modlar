5c06add3cd324278a1d754e710797185263c6869
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-12 15:40:38 +0200

    :apple: Implement navigationWatcher

--- ukeplanr_template/lib/logic/navigation/observers/navigationWatcher.dart
+++ ukeplanr_template/lib/logic/navigation/observers/navigationWatcher.dart
@@ -7,4 +7,10 @@ class NavigationWatcher extends NavigatorObserver {
   void didReplace({Route? newRoute, Route? oldRoute}) {
     print("ok so something replaced");
   }
+
+  List<Map<String?, RouteSettings>> _routeHistory = [];
+
+  List<Map<String?, RouteSettings>> get getRouteHistory => _routeHistory;
+  void registerRoute(RouteSettings settings) =>
+      _routeHistory.add({settings.name: settings});
 }

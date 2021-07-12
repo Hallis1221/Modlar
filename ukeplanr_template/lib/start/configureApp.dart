3b758959401572cf786a71794265d13bfcc30792
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-12 15:40:57 +0200

    :truck: Moved configureRoutes.dart

--- ukeplanr_template/lib/start/configureApp.dart
+++ ukeplanr_template/lib/start/configureApp.dart
@@ -1,4 +1,4 @@
-import 'package:ukeplanr_template/start/routes/configureRoutes.dart';
+import 'package:ukeplanr_template/start/navigation/routes/configureRoutes.dart';
 import 'package:ukeplanr_template/start/widgets/configureInitialization.dart';
 import 'package:ukeplanr_template/start/logs/configerLogger.dart';
 import 'package:ukeplanr_template/start/theme/configureTheme.dart';

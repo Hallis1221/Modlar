f2e8b2184f6e74de9a7b9e55bc4b4cb7b6262b15
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-12 15:53:32 +0200

    :100: Register route when passed to the generateRoute function

--- ukeplanr_template/lib/logic/navigation/gen/generateRoute.dart
+++ ukeplanr_template/lib/logic/navigation/gen/generateRoute.dart
@@ -1,7 +1,20 @@
-import 'package:flutter/material.dart';
+import 'package:ukeplanr_template/logic/navigation/observers/navigationWatcher.dart';
 import 'package:ukeplanr_template/logic/navigation/mapping/findWidget.dart';
+import 'package:ukeplanr_template/logic/logs/logService.dart';
+
+import 'package:get_it/get_it.dart';
+import 'package:flutter/material.dart';
 
 Route generateRoute(RouteSettings settings) {
+  final Function? log = GetIt.instance.get<LogsService>().logger!.log;
+
+  try {
+    GetIt.instance<NavigationWatcher>().registerRoute(settings);
+  } catch (e) {
+    log!("""
+        Tried to register ${settings.name} inside NavigationWatcher but got the following error: $e 
+        """);
+  }
   return MaterialPageRoute(
     builder: (context) => findWidgetFromRoute(settings: settings),
   );

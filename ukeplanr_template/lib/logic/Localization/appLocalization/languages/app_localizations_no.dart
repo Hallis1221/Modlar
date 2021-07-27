aee4cd67e0d3d9bd371dad5e1f44ba3a00afe521
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-27 14:59:45 +0200

    :radio_button: Add generated language files

---
+++ ukeplanr_template/lib/logic/Localization/appLocalization/languages/app_localizations_no.dart
@@ -0,0 +1,13 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import '../app_localizations.dart';
+
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+/// The translations for Norwegian (`no`).
+class AppLocalizationsNo extends AppLocalizations {
+  AppLocalizationsNo([String locale = 'no']) : super(locale);
+
+  @override
+  String get helloWorld => 'Hei verden!';
+}

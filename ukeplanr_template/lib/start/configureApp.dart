44abec5a25012a406656ce8c9284d197e45f2f34
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-31 18:59:17 +0200

    :calling: Call configureLocalization();

--- ukeplanr_template/lib/start/configureApp.dart
+++ ukeplanr_template/lib/start/configureApp.dart
@@ -1,3 +1,4 @@
+import 'package:ukeplanr_template/start/localization/configureLocalization.dart';
 import 'package:ukeplanr_template/start/navigation/configureNavigation.dart';
 import 'package:ukeplanr_template/start/navigation/routes/configureRoutes.dart';
 import 'package:ukeplanr_template/start/widgets/configureInitialization.dart';
@@ -7,6 +8,7 @@ import 'package:ukeplanr_template/start/theme/configureTheme.dart';
 configureApp() {
   configureLogger();
   configureNavigationWatcher();
+  configureLocalization();
   configureRoutes();
   configureInitialization();
   configureThemes();

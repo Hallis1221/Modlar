43cc9c19ae5d6b34bfbad2217efbca883bb85e2f
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-31 19:34:39 +0200

    :1234: Change _sessionLogs to use behaviorsubjects

--- ukeplanr_template/lib/logic/logs/output/logOutput.dart
+++ ukeplanr_template/lib/logic/logs/output/logOutput.dart
@@ -1,10 +1,24 @@
+import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
+import 'package:rxdart/subjects.dart';
 
 class LogSession extends LogOutput {
+  BehaviorSubject<List<String>> _sessionLogs = BehaviorSubject.seeded([]);
+
+  List<String> get getSessionLog => _sessionLogs.value;
+
+  addLogToSession(String log) => _sessionLogs.value.add(log);
+
   @override
   void output(OutputEvent event) {
+    String log = "";
+
     for (var line in event.lines) {
       print(line);
+      log += "$line \n";
     }
+    GetIt.instance.get<LogSession>().addLogToSession(log);
   }
+
+  LogSession();
 }

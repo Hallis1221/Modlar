8e2a5032aad053177119fe361410a7035c547a75
Hallis - home main <halvorvivelstad@gmail.com>
2021-07-31 23:34:52 +0200

    :truck: Move 404.dart

--- ukeplanr_template/lib/logic/navigation/mapping/findWidget.dart
+++ ukeplanr_template/lib/logic/navigation/mapping/findWidget.dart
@@ -1,4 +1,4 @@
-import 'package:ukeplanr_template/components/temp/errors/404.dart';
+import 'package:ukeplanr_template/components/temp/nodesign/errors/404.dart';
 import 'package:ukeplanr_template/logic/navigation/routes/routes.dart';
 
 import 'package:flutter/widgets.dart';

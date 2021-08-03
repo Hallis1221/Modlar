3ea7cda474405e8073d7bf16eb875966e15186a8
Hallis1221 <halvorviv@gmail.com>
2021-08-03 23:24:36 +0200

    :1234: Add a way to get the current theme, set it and add a new theme to the themeservice

--- ukeplanr_template/lib/logic/theme/themes.dart
+++ ukeplanr_template/lib/logic/theme/themes.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
 class ThemesService {
   final Map<String?, ThemeData?> themes;
-
   Map<String?, ThemeData?> get themesList => themes;
+
+  // ignore: close_sinks
+  final BehaviorSubject<ThemeData?> currentTheme;
+
+  setCurrentTheme(String themeName) {
+    currentTheme.value = this.findTheme(themeName);
+  }
+
+  addTheme(ThemeData theme, String themeName) {
+    this.themesList[themeName] = theme;
+  }
 
   ThemeData? findTheme(String themeName) {
     return this.themesList[themeName];
   }
 
-  ThemesService({required this.themes});
+  ThemesService({required this.themes, required this.currentTheme});
 }

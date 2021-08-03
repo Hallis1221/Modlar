a248dd3c4045be53e1ab1a4154b22c3f523d3848
Hallis1221 <halvorviv@gmail.com>
2021-08-03 23:23:37 +0200

    :innocent: Include the new themecreator in routs

--- ukeplanr_template/lib/config/routes/routes.dart
+++ ukeplanr_template/lib/config/routes/routes.dart
@@ -2,11 +2,13 @@ import 'package:ukeplanr_template/components/testing/debug/logsUI.dart';
 import 'package:ukeplanr_template/components/testing/placeholder/helloWorld.dart';
 
 import 'package:flutter/widgets.dart';
+import 'package:ukeplanr_template/components/testing/theme/newTheme.dart';
 
 class RoutesConfig {
   final Map<String, Widget> _routes = {
     "/": PlaceholderWidget(),
     "/session": DebugLogsUI(),
+    "/theme": ThemeCreator(),
   };
   Map<String, Widget> get getRoutes => _routes;
 }

fd6e64110371a30c4b7bbb3767d897f8f022588d
Hallis1221 <halvorviv@gmail.com>
2021-08-04 17:25:44 +0200

    :sake: Save local preference when its changed

--- ukeplanr_template/lib/logic/localization/state/locale.dart
+++ ukeplanr_template/lib/logic/localization/state/locale.dart
@@ -1,3 +1,4 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ukeplanr_template/logic/logs/printer/logService.dart';
 
 import 'package:flutter/material.dart';
@@ -6,20 +7,22 @@ import 'package:get_it/get_it.dart';
 import 'package:rxdart/rxdart.dart';
 
 class LocaleName {
-  final Function? log = GetIt.instance.get<LogsService>().logger!.log;
-
   // ignore: close_sinks
   BehaviorSubject<Locale?> _localeName = BehaviorSubject.seeded(null);
 
   Stream<Locale?>? get stream$ => _localeName.stream;
 
-  changeLocale(Locale locale) {
+  changeLocale(Locale locale) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final Function? _log = GetIt.instance.get<LogsService>().logger!.log;
+
     if (_localeName.value == locale) {
-      log!(Level.error,
+      _log!(Level.error,
           "Locale was not changed beacuse locale already is $locale. ($locale is the same as ${_localeName.value})");
     } else {
-      log!(Level.debug, "Changed locale from ${_localeName.value} to $locale");
+      _log!(Level.debug, "Changed locale from ${_localeName.value} to $locale");
       _localeName.value = locale;
+      prefs.setString("activeLocale", locale.toString());
     }
   }
 

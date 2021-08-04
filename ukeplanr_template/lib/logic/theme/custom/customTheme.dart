348067a65435e1346cebbd10f17a431f00819f2e
Hallis1221 <halvorviv@gmail.com>
2021-08-04 15:28:59 +0200

    :100: Make using CustomTheme easier as you dont have to give it all of its value instead you just have to give it the buildcontext

--- ukeplanr_template/lib/logic/theme/custom/customTheme.dart
+++ ukeplanr_template/lib/logic/theme/custom/customTheme.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
+// !! Each time a new property is added to custom theme it also have to be added to
+// !! the toMap extension in extensions/themeData/asMap.dart.
 class CustomTheme {
-  BehaviorSubject<Color> backgroundColor;
-  BehaviorSubject<Color> buttonColor;
+  // ignore_for_file: close_sinks
+  BuildContext context;
+  late BehaviorSubject<Color> backgroundColor =
+      BehaviorSubject.seeded(Theme.of(context).backgroundColor);
+  late BehaviorSubject<Color> buttonColor =
+      BehaviorSubject.seeded(Theme.of(context).buttonColor);
 
-  CustomTheme({required this.backgroundColor, required this.buttonColor});
+  CustomTheme({required this.context});
 }

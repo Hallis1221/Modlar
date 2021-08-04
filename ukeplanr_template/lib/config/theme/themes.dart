47fcc68dfe0bce75e71b260cdf709d818717006a
Hallis1221 <halvorviv@gmail.com>
2021-08-04 17:23:34 +0200

    :sa: Save a prefix for customthemes in the theme config file

--- ukeplanr_template/lib/config/theme/themes.dart
+++ ukeplanr_template/lib/config/theme/themes.dart
@@ -4,7 +4,7 @@ class ThemeConfig {
   final ThemeData _orangeTheme = ThemeData(
     backgroundColor: Colors.orange,
   );
-
   late final Map<String, ThemeData> themes = {"orange": _orangeTheme};
+  final String customThemePrefix = "customTheme_";
   Map<String, ThemeData> get getAllThemes => themes;
 }

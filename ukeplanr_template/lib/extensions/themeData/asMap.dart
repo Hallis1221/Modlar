f78b0311ccb0913880caca3b00519b0afe68be3b
Hallis1221 <halvorviv@gmail.com>
2021-08-04 17:24:22 +0200

    :chicken: Changed the name of .toHex to .toExportableString

--- ukeplanr_template/lib/extensions/themeData/asMap.dart
+++ ukeplanr_template/lib/extensions/themeData/asMap.dart
@@ -1,12 +1,12 @@
-import 'package:ukeplanr_template/extensions/color/asHex.dart';
+import 'package:ukeplanr_template/extensions/color/asExportableString.dart';
 
 import 'package:flutter/material.dart';
 
 extension ConvertToMap on ThemeData {
   Map<String, String> toMap() {
     return {
-      "backgroundColor": this.backgroundColor.toHex(),
-      "buttonColor": this.buttonColor.toHex()
+      "backgroundColor": this.backgroundColor.toExportableString(),
+      "buttonColor": this.buttonColor.toExportableString()
     };
   }
 }

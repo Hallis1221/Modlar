12057b4e3bac16f552f47dea95f041c0ced94d21
Hallis1221 <halvorviv@gmail.com>
2021-08-04 17:27:06 +0200

    :8ball: Make configureLocalization able to get the active locale

--- ukeplanr_template/lib/start/localization/configureLocalization.dart
+++ ukeplanr_template/lib/start/localization/configureLocalization.dart
@@ -1,25 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:ukeplanr_template/logic/localization/state/locale.dart';
 import 'package:ukeplanr_template/logic/logs/printer/logService.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
 
-configureLocalization() {
+configureLocalization() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  final Function? log = GetIt.instance.get<LogsService>().logger!.log;
+
   try {
     GetIt.instance.registerSingleton<LocaleName>(
       LocaleName(),
     );
+    try {
+      String? storedLocale = prefs.getString("activeLocale");
+      if (storedLocale != null) {
+        List localeStrings = storedLocale.split("_");
+        GetIt.instance
+            .get<LocaleName>()
+            .changeLocale(Locale(localeStrings[0], localeStrings[1]));
+      }
+    } catch (error) {
+      log!(Level.error,
+          "Tried to get and use the active locale but got the following error: $error");
+    }
 
     try {
-      final Function? log = GetIt.instance.get<LogsService>().logger!.log;
       log!(Level.info, "Configured LocaleName!");
     } catch (e) {
       print(e);
     }
   } catch (e) {
     try {
-      final Function? log = GetIt.instance.get<LogsService>().logger!.log;
       log!(Level.error, """
           Failed to configure LocaleName! The following information was given: $e
           """);

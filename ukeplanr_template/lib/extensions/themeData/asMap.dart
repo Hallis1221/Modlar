4058ec1473deec476c8e18412ab03d87da5189a7
Hallis1221 <halvorviv@gmail.com>
2021-08-05 16:35:57 +0200

    :1234: Make asTheme and asMap to support all cases of theme colors

--- ukeplanr_template/lib/extensions/themeData/asMap.dart
+++ ukeplanr_template/lib/extensions/themeData/asMap.dart
@@ -5,8 +5,32 @@ import 'package:flutter/material.dart';
 extension ConvertToMap on ThemeData {
   Map<String, String> toMap() {
     return {
+      "dialogBackgroundColor": this.dialogBackgroundColor.toExportableString(),
+      "toggleableActiveColor": this.toggleableActiveColor.toExportableString(),
+      "unselectedWidgetColor": this.unselectedWidgetColor.toExportableString(),
+      "secondaryHeaderColor": this.secondaryHeaderColor.toExportableString(),
+      "primaryColorLight": this.primaryColorLight.toExportableString(),
+      "bottomAppBarColor": this.bottomAppBarColor.toExportableString(),
+      "selectedRowColor": this.selectedRowColor.toExportableString(),
+      "primaryColorDark": this.primaryColorDark.toExportableString(),
       "backgroundColor": this.backgroundColor.toExportableString(),
-      "buttonColor": this.buttonColor.toExportableString()
+      "highlightColor": this.highlightColor.toExportableString(),
+      "indicatorColor": this.indicatorColor.toExportableString(),
+      "disabledColor": this.disabledColor.toExportableString(),
+      "dividerColor": this.dividerColor.toExportableString(),
+      "primaryColor": this.primaryColor.toExportableString(),
+      "scaffoldBackgroundColor":
+          this.scaffoldBackgroundColor.toExportableString(),
+      "shadowColor": this.shadowColor.toExportableString(),
+      "splashColor": this.splashColor.toExportableString(),
+      "canvasColor": this.canvasColor.toExportableString(),
+      "buttonColor": this.buttonColor.toExportableString(),
+      "accentColor": this.accentColor.toExportableString(),
+      "errorColor": this.errorColor.toExportableString(),
+      "focusColor": this.focusColor.toExportableString(),
+      "hoverColor": this.hoverColor.toExportableString(),
+      "hintColor": this.hintColor.toExportableString(),
+      "cardColor": this.cardColor.toExportableString(),
     };
   }
 }

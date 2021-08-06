ccf857fff7d036525ad37156c804ed96618f4e6b
Hallis1221 <halvorviv@gmail.com>
2021-08-06 15:08:54 +0200

    :100: Add textTheme property to CustomTheme

--- ukeplanr_template/lib/logic/theme/custom/customTheme.dart
+++ ukeplanr_template/lib/logic/theme/custom/customTheme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:ukeplanr_template/logic/theme/custom/customTextTheme.dart';
 
 // !! Each time a new property is added to custom theme it also have to be added to
 // !! the toMap extension in extensions/themeData/asMap.dart. and extensios/map/asTheme.dart
@@ -10,6 +11,11 @@ class CustomTheme {
       BehaviorSubject.seeded(Theme.of(context).backgroundColor);
   late BehaviorSubject<Color> buttonColor =
       BehaviorSubject.seeded(Theme.of(context).buttonColor);
+  late BehaviorSubject<CustomTextTheme> textTheme = BehaviorSubject.seeded(
+    new CustomTextTheme(
+      textTheme: Theme.of(context).textTheme,
+    ),
+  );
 
   CustomTheme({required this.context});
 }

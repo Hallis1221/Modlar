e01dafaa2bec59efbb829d27a635132d829a276b
Hallis1221 <halvorviv@gmail.com>
2021-08-07 11:02:17 +0200

    :x: Remove textchanger (bc its not needed) and added localization to the done text

--- ukeplanr_template/lib/UI/screens/testing/theme/newTheme.dart
+++ ukeplanr_template/lib/UI/screens/testing/theme/newTheme.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/services.dart';
-import 'package:ukeplanr_template/logic/theme/custom/customTextTheme.dart';
 import 'package:ukeplanr_template/logic/theme/custom/customTheme.dart';
 import 'package:ukeplanr_template/UI/components/theme/colorPicker.dart';
 import 'package:ukeplanr_template/logic/theme/themes.dart';
-import 'package:ukeplanr_template/extensions/textStyle/setSize.dart';
-import 'package:ukeplanr_template/extensions/customTextTheme/toTextTheme.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
@@ -35,78 +31,24 @@ class ThemeCreator extends StatelessWidget {
               },
               title: AppLocalizations.of(context)!.changeButtonColor,
             ),
-            _TextChanger(
-              textTheme: customTheme.textTheme,
-            ),
             MaterialButton(
-              child: Text("Done"),
+              child: Text(AppLocalizations.of(context)!.done),
               onPressed: () {
                 print(customTheme.textTheme.value.bodyText1.toString());
                 ThemesService themesServiceInstance =
                     GetIt.instance.get<ThemesService>();
                 themesServiceInstance.addTheme(
                     ThemeData(
-                        backgroundColor: customTheme.backgroundColor.value,
-                        buttonColor: customTheme.buttonColor.value,
-                        // !! TODO throws exception
-                        textTheme: new TextTheme(
-                            bodyText1:
-                                customTheme.textTheme.value.bodyText1.value)),
+                      backgroundColor: customTheme.backgroundColor.value,
+                      buttonColor: customTheme.buttonColor.value,
+                      // !! TODO throws exception
+                    ),
                     "custom");
                 themesServiceInstance.saveAndSetTheme("custom");
               },
             )
           ],
         ),
-      ),
-    );
-  }
-}
-
-// TODO make a text changer for changin textstyles
-class _TextChanger extends StatelessWidget {
-  const _TextChanger({
-    Key? key,
-    required this.textTheme,
-  }) : super(key: key);
-  final BehaviorSubject<CustomTextTheme> textTheme;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 100,
-      child: Row(
-        children: [
-          StreamBuilder<TextStyle?>(
-              stream: textTheme.value.bodyText1.stream,
-              builder: (context, snapshot) {
-                return Container(
-                  height: 100,
-                  width: 100,
-                  child: new TextField(
-                    decoration: new InputDecoration(
-                        labelText: "Size", labelStyle: snapshot.data),
-                    keyboardType: TextInputType.number,
-
-                    onChanged: (value) {
-                      textTheme.value.bodyText1.value = textTheme
-                          .value.bodyText1.value
-                          .setSize(int.parse(value));
-                    },
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.digitsOnly,
-                      TextInputFormatter.withFunction((oldValue, newValue) {
-                        if (int.parse(newValue.text) >= 144) {
-                          return oldValue;
-                        } else {
-                          return newValue;
-                        }
-                      })
-                    ], // Only numbers can be entered
-                  ),
-                );
-              }),
-        ],
       ),
     );
   }

c35dde89cda6a5ce07813acca06cd5c911689c30
Hallis1221 <halvorviv@gmail.com>
2021-08-07 11:03:21 +0200

    :gift: Give the home page the ability to change language back and forth

--- ukeplanr_template/lib/UI/screens/testing/home/helloWorld.dart
+++ ukeplanr_template/lib/UI/screens/testing/home/helloWorld.dart
@@ -1,7 +1,9 @@
+import 'package:get_it/get_it.dart';
 import 'package:ukeplanr_template/UI/components/locale/changeLocale.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:ukeplanr_template/logic/Localization/state/locale.dart';
 
 class PlaceholderWidget extends StatelessWidget {
   const PlaceholderWidget({
@@ -10,6 +12,7 @@ class PlaceholderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(Localizations.localeOf(context).toString());
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
       body: Center(
@@ -22,7 +25,9 @@ class PlaceholderWidget extends StatelessWidget {
                 style: Theme.of(context).textTheme.bodyText1,
               ),
               ChangeLocale(
-                newLocale: Locale('no', 'NB'),
+                newLocale: Localizations.localeOf(context).toString() == "en_"
+                    ? Locale("no", "NB")
+                    : Locale("en", "US"),
               ),
             ],
           ),

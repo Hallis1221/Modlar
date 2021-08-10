72256995abf9bb12962afbc228e852177c359518
Hallis1221 <halvorviv@gmail.com>
2021-08-10 14:15:32 +0200

    :x: Remove not needed print()

--- ukeplanr_template/lib/logic/navigation/observers/navigationWatcher.dart
+++ ukeplanr_template/lib/logic/navigation/observers/navigationWatcher.dart
@@ -4,9 +4,7 @@ class NavigationWatcher extends NavigatorObserver {
   // todo implement custom tracking feature for pages
 
   @override
-  void didReplace({Route? newRoute, Route? oldRoute}) {
-    print("ok so something replaced");
-  }
+  void didReplace({Route? newRoute, Route? oldRoute}) {}
 
   List<Map<String?, RouteSettings>> _routeHistory = [];
 

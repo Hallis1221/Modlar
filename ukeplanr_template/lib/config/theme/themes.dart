89a188e4a3b7c82b57c50dc6591e2b2f1de7f1f2
Hallis1221 <halvorviv@gmail.com>
2021-08-10 14:15:01 +0200

    :1234: Autoformatting for themes.dart

--- ukeplanr_template/lib/config/theme/themes.dart
+++ ukeplanr_template/lib/config/theme/themes.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ThemeConfig {
-  final ThemeData _orangeTheme =
-      ThemeData(backgroundColor: Colors.orange, textTheme: TextTheme());
+  final ThemeData _orangeTheme = ThemeData(
+    backgroundColor: Colors.orange,
+    textTheme: TextTheme(),
+  );
+
+  // All the pre made themes and their name
   late final Map<String, ThemeData> themes = {"orange": _orangeTheme};
+
   final String customThemePrefix = "customTheme_";
   Map<String, ThemeData> get getAllThemes => themes;
 }

4f7129c626b6f39c278a563c4793bea7b8dda30f
Hallis1221 <halvorviv@gmail.com>
2021-08-10 14:22:21 +0200

    :spades: Add multiple theme saving

--- ukeplanr_template/lib/UI/screens/testing/theme/newTheme.dart
+++ ukeplanr_template/lib/UI/screens/testing/theme/newTheme.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:ukeplanr_template/logic/theme/custom/customTheme.dart';
 import 'package:ukeplanr_template/UI/components/theme/colorPicker.dart';
 import 'package:ukeplanr_template/logic/theme/themes.dart';
@@ -12,45 +14,112 @@ class ThemeCreator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    CustomTheme customTheme = new CustomTheme(context: context);
-    return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            _ColorChanger(
-              color: customTheme.backgroundColor,
-              title: AppLocalizations.of(context)!.changeBackgroundColor,
-              onChange: (Color color) {
-                customTheme.backgroundColor.value = color;
-              },
-            ),
-            _ColorChanger(
-              color: customTheme.buttonColor,
-              onChange: (Color color) {
-                customTheme.buttonColor.value = color;
-              },
-              title: AppLocalizations.of(context)!.changeButtonColor,
-            ),
-            MaterialButton(
-              child: Text(AppLocalizations.of(context)!.done),
-              onPressed: () {
-                print(customTheme.textTheme.value.bodyText1.toString());
-                ThemesService themesServiceInstance =
-                    GetIt.instance.get<ThemesService>();
-                themesServiceInstance.addTheme(
-                    ThemeData(
-                      backgroundColor: customTheme.backgroundColor.value,
-                      buttonColor: customTheme.buttonColor.value,
-                      // !! TODO throws exception
+    // ignore: close_sinks
+    BehaviorSubject<CustomTheme> customTheme = BehaviorSubject.seeded(
+        new CustomTheme(
+            theme: Theme.of(context),
+            themeName: GetIt.instance.get<ThemesService>().currentThemeName));
+    TextEditingController nameController = TextEditingController(
+        text: GetIt.instance.get<ThemesService>().currentThemeName);
+    return StreamBuilder<CustomTheme>(
+        stream: customTheme.stream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active)
+            return Scaffold(
+              backgroundColor: snapshot.data!.backgroundColor.value,
+              body: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 50,
+                          child: TextField(
+                            controller: nameController,
+                          ),
+                        ),
+                        _ColorChanger(
+                          color: snapshot.data!.backgroundColor,
+                          title: AppLocalizations.of(context)!
+                              .changeBackgroundColor,
+                          onChange: (Color color) {
+                            customTheme.value.backgroundColor.value = color;
+                            customTheme.value = customTheme.value;
+                          },
+                        ),
+                        _ColorChanger(
+                          color: snapshot.data!.buttonColor,
+                          onChange: (Color color) {
+                            customTheme.value.buttonColor.value = color;
+                          },
+                          title:
+                              AppLocalizations.of(context)!.changeButtonColor,
+                        ),
+                        MaterialButton(
+                          child: Text(
+                            AppLocalizations.of(context)!.done,
+                          ),
+                          textColor: useWhiteForeground(
+                                  snapshot.data!.backgroundColor.value)
+                              ? Colors.white
+                              : Colors.black,
+                          onPressed: () {
+                            ThemesService themesServiceInstance =
+                                GetIt.instance.get<ThemesService>();
+
+                            themesServiceInstance.addTheme(
+                                ThemeData(
+                                  backgroundColor:
+                                      snapshot.data!.backgroundColor.value,
+                                  buttonColor: snapshot.data!.buttonColor.value,
+                                  // !! TODO throws exception
+                                ),
+                                nameController.text);
+                            themesServiceInstance
+                                .saveAndSetTheme(nameController.text);
+                          },
+                        )
+                      ],
                     ),
-                    "custom");
-                themesServiceInstance.saveAndSetTheme("custom");
-              },
-            )
-          ],
-        ),
-      ),
-    );
+                  ),
+                  DropdownButton(
+                      value: snapshot.data!.themeName,
+                      isDense: false,
+                      onChanged: (String? value) {
+                        customTheme.value.themeName = value.toString();
+                        ThemeData? themeData = GetIt.instance
+                            .get<ThemesService>()
+                            .findTheme(value!);
+                        nameController.text = value;
+                        customTheme.value = CustomTheme(
+                          theme: ThemeData(
+                            backgroundColor: themeData!.backgroundColor,
+                            buttonColor: themeData.buttonColor,
+                          ),
+                          themeName: value,
+                        );
+                        customTheme = customTheme;
+                      },
+                      items: <DropdownMenuItem<String>>[
+                        for (String? themeName in GetIt.instance
+                            .get<ThemesService>()
+                            .themes
+                            .keys
+                            .toSet()
+                            .toList())
+                          DropdownMenuItem(
+                              value: themeName.toString(),
+                              child: Text(themeName.toString())),
+                      ]),
+                ],
+              ),
+            );
+          else
+            return Container();
+        });
   }
 }
 

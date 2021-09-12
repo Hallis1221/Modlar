2314ad92b0f8f148202718cb99da430240a617a5
Halvor Vivelstad <halvorviv@gmail.com>
2021-09-12 18:57:50 +0200

    :red_circle: Create a seperate widget to make it easier to create colored backgrounds

---
+++ ukeplanr_template/lib/UI/components/backgrounds/glassedColor.dart
@@ -0,0 +1,42 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:ukeplanr_template/UI/components/backgrounds/glass.dart';
+import 'package:ukeplanr_template/UI/components/off-center/offCenterCircle.dart';
+
+class GlassedColor extends StatelessWidget {
+  const GlassedColor({
+    Key? key,
+    this.blur = 120,
+    this.child,
+    this.colors = const [],
+    this.opacity = 0.5,
+  });
+
+  final double blur;
+  final Widget? child;
+  final List<OffCenterCircle> colors;
+  final double opacity;
+
+  @override
+  Widget build(BuildContext context) {
+    return GlassBackground(
+      blur: this.blur,
+      child: Container(
+        child: this.child,
+      ),
+      backgroundChild: Center(
+        child: Container(
+          decoration: new BoxDecoration(
+            color: Colors.grey.shade200.withOpacity(
+              this.opacity,
+            ),
+          ),
+          child: Stack(
+            children: this.colors,
+          ),
+        ),
+      ),
+    );
+  }
+}

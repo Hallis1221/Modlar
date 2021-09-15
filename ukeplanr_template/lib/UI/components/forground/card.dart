ccc490b927d7e63edef1c36107f6e8883e0845ee
Halvor Vivelstad <halvorviv@gmail.com>
2021-09-15 18:45:19 +0200

    :love_letter: Add a card widget

---
+++ ukeplanr_template/lib/UI/components/forground/card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ClassCard extends StatelessWidget {
+  const ClassCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          color: Colors.red,
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Card with circular border',
+                textScaleFactor: 1.2,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

88a6b3af0bfed3541437a99a101399c6d84ce487
Halvor Vivelstad <halvorviv@gmail.com>
2021-09-15 18:45:46 +0200

    :1234: Try animation

--- ukeplanr_template/lib/UI/screens/testing/styles/glassBackgroundTest.dart
+++ ukeplanr_template/lib/UI/screens/testing/styles/glassBackgroundTest.dart
@@ -1,10 +1,9 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:ukeplanr_template/UI/components/backgrounds/glassedColor.dart';
+import 'package:ukeplanr_template/UI/components/forground/card.dart';
 import 'package:ukeplanr_template/UI/components/off-center/offCenterCircle.dart';
-import 'package:ukeplanr_template/extensions/color/fromHex.dart';
-import 'package:ukeplanr_template/UI/components/backgrounds/glass.dart';
+import 'package:ukeplanr_template/logic/animation/background.dart';
 
 class GlassBackgroundPage extends StatelessWidget {
   const GlassBackgroundPage({Key? key}) : super(key: key);
@@ -14,11 +13,35 @@ class GlassBackgroundPage extends StatelessWidget {
     return Scaffold(
       body: Container(
           child: GlassedColor(
+        child: MaterialButton(
+            onPressed: () {
+              GetIt.instance.get<BackgroundColorAnimation>().setVarOne(15);
+            },
+            child: ClassCard()),
         colors: [
-          OffCenterCircle(
-            color: Colors.red,
-            alignment: Alignment(-1.5, 1.5),
-          ),
+          StreamBuilder<num>(
+              stream: GetIt.instance.get<BackgroundColorAnimation>().stream$,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData || snapshot.data == null)
+                  return OffCenterCircle(
+                    color: Colors.blue,
+                    alignment: Alignment(-1.5, 1.5),
+                  );
+                num thing = -1;
+                Color clr = Colors.red;
+                if (snapshot.requireData >= 10) {
+                  thing = 1;
+                  clr = Colors.black;
+                }
+                print(thing);
+                return AnimatedContainer(
+                  duration: const Duration(seconds: 20),
+                  child: OffCenterCircle(
+                    color: clr,
+                    alignment: Alignment(1.5 * thing, 1.5),
+                  ),
+                );
+              }),
           OffCenterCircle(
             color: Colors.blue,
             alignment: Alignment(1, -1.5),

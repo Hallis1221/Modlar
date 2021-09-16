d5bd3139910a6953b979fa312535a7a682476e45
Halvor Vivelstad <halvorviv@gmail.com>
2021-09-16 22:29:51 +0200

    :100: Add logic to show the banner if its enabled on all pages

--- ukeplanr_template/lib/logic/navigation/mapping/findWidget.dart
+++ ukeplanr_template/lib/logic/navigation/mapping/findWidget.dart
@@ -1,3 +1,5 @@
+import 'package:ukeplanr_template/UI/components/dev/runModeIndicator.dart';
+import 'package:ukeplanr_template/logic/dev/mode.dart';
 import 'package:ukeplanr_template/logic/navigation/routes/routes.dart';
 import 'package:ukeplanr_template/UI/components/errors/404.dart';
 
@@ -9,8 +11,15 @@ Widget findWidgetFromRoute({
 }) {
   Widget? nullableWidget =
       GetIt.instance<RoutesService>().getRoutes[settings!.name];
+  RunMode runMode = GetIt.instance<RunMode>();
   Widget widget = nullableWidget != null
       ? nullableWidget
       : GetIt.instance<RoutesService>().get404Widget;
+
+  // if the app has a banner enabled, show it.
+  if (runMode.showBanner)
+    return Stack(
+      children: [widget, RunModeBanner(runMode.runMode)],
+    );
   return widget;
 }

61d61916f65cb2c6ea8ec1d53b187fdfbccbbcc1
Halvor Vivelstad <halvorviv@gmail.com>
2021-09-16 22:30:21 +0200

    :abcd: Config file to register runMode in getit

---
+++ ukeplanr_template/lib/start/dev/configureMode.dart
@@ -0,0 +1,33 @@
+// import 'package:ukeplanr_template/generated_plugin_registrant.dart';
+import 'package:ukeplanr_template/config/dev/dev.dart';
+import 'package:ukeplanr_template/logic/dev/mode.dart';
+import 'package:ukeplanr_template/logic/logs/printer/logService.dart';
+
+import 'package:flutter/foundation.dart';
+import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
+
+void configureRunMode() {
+  try {
+    GetIt.instance.registerSingleton<RunMode>(
+      RunMode(
+        showBanner: (RunModeConfig().runMode == "") ? false : true,
+        runMode: RunModeConfig().runMode,
+      ),
+    );
+    try {
+      final Function? log = GetIt.instance.get<LogsService>().logger!.log;
+      log!(Level.info, "Configured shared preference plugins!");
+    } catch (e) {}
+  } catch (e) {
+    try {
+      final Function? log = GetIt.instance.get<LogsService>().logger!.log;
+      log!(Level.error, """
+          Failed to configure shared preference plugins! The following information was given: $e
+          """);
+    } catch (e) {
+      throw FlutterError(
+          "Failed to configure shared preference plugins and failed to log the exception! The error was: $e");
+    }
+  }
+}

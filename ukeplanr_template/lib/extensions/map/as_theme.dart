8be29dff356a40c3651bce57151369d7ff791577
Hallis1221 <46447358+Hallis1221@users.noreply.github.com>
2021-09-21 14:05:20 +0200

    :ox: Convert map to ColorScheme();

--- ukeplanr_template/lib/extensions/map/as_theme.dart
+++ ukeplanr_template/lib/extensions/map/as_theme.dart
@@ -29,7 +29,25 @@ extension ConvertToTheme on Map {
       hoverColor: Color(int.parse(this["hoverColor"])),
       hintColor: Color(int.parse(this["hintColor"])),
       cardColor: Color(int.parse(this["cardColor"])),
-      //colorScheme: ColorScheme(),
+      colorScheme: this["colorScheme"].toColorScheme(),
+    );
+  }
+
+  ColorScheme toColorScheme() {
+    return ColorScheme(
+      brightness: this["brightness"],
+      background: this["background"],
+      error: this["error"],
+      onBackground: this["onBackground"],
+      onError: this["onError"],
+      onPrimary: this["onPrimary"],
+      onSecondary: this["onSecondary"],
+      onSurface: this["onSurface"],
+      primary: this["primary"],
+      primaryVariant: this["primaryVariant"],
+      secondary: this["secondary"],
+      secondaryVariant: this["secondaryVariant"],
+      surface: this["surface"],
     );
   }
 }

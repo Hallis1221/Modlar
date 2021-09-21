f5af56c854f88389d3b5cf492b11225793e09816
Hallis1221 <46447358+Hallis1221@users.noreply.github.com>
2021-09-21 12:14:46 +0200

    :x: Remove deprecated accentColor & buttoncolor and add colorScheme

--- ukeplanr_template/lib/logic/theme/custom/custom_theme.dart
+++ ukeplanr_template/lib/logic/theme/custom/custom_theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:ukeplanr_template/logic/theme/custom/custom_text_theme.dart';
+import 'package:ukeplanr_template/logic/theme/custom/custom_color_scheme.dart';
 
 // !! Each time a new property is added to custom theme it also have to be added to
 // !! the toMap extension in extensions/themeData/asMap.dart. and extensios/map/asTheme.dart
@@ -9,16 +9,14 @@ class CustomTheme {
   String themeName;
   late BehaviorSubject<Color> backgroundColor =
       BehaviorSubject<Color>.seeded(theme.backgroundColor);
-  late BehaviorSubject<Color> buttonColor =
-      BehaviorSubject<Color>.seeded(theme.buttonColor);
+
   late BehaviorSubject<Color> primaryColor =
       BehaviorSubject<Color>.seeded(theme.primaryColor);
 
-  @Deprecated("This feature is not fully implemented fully")
-  late BehaviorSubject<CustomTextTheme> textdTheme =
-      BehaviorSubject<CustomTextTheme>.seeded(
-    CustomTextTheme(
-      textTheme: theme.textTheme,
+  late BehaviorSubject<CustomColorScheme> colorScheme =
+      BehaviorSubject<CustomColorScheme>.seeded(
+    CustomColorScheme(
+      colorScheme: theme.colorScheme,
     ),
   );
 

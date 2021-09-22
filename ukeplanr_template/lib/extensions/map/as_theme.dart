6133995387394be3881ba366bb3c57b44a0df862
Hallis1221 <46447358+Hallis1221@users.noreply.github.com>
2021-09-22 12:30:11 +0200

    :punch: Put toColorScheme in its own file
    its cleaner

--- ukeplanr_template/lib/extensions/map/as_theme.dart
+++ ukeplanr_template/lib/extensions/map/as_theme.dart
@@ -32,22 +32,4 @@ extension ConvertToTheme on Map {
       colorScheme: this["colorScheme"].toColorScheme(),
     );
   }
-
-  ColorScheme toColorScheme() {
-    return ColorScheme(
-      brightness: this["brightness"],
-      background: this["background"],
-      error: this["error"],
-      onBackground: this["onBackground"],
-      onError: this["onError"],
-      onPrimary: this["onPrimary"],
-      onSecondary: this["onSecondary"],
-      onSurface: this["onSurface"],
-      primary: this["primary"],
-      primaryVariant: this["primaryVariant"],
-      secondary: this["secondary"],
-      secondaryVariant: this["secondaryVariant"],
-      surface: this["surface"],
-    );
-  }
 }

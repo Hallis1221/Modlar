780fb50ecc1a0529f6fc21e9e7376b795d5c577a
Halvor Vivelstad <halvorviv@gmail.com>
2021-10-02 17:09:04 +0200

    :cd: Convert string to int to color in toColorScheme()

--- ukeplanr_template/lib/extensions/map/as_color_scheme.dart
+++ ukeplanr_template/lib/extensions/map/as_color_scheme.dart
@@ -5,19 +5,19 @@ import 'package:flutter/material.dart';
 extension ConvertToColorScheme on Map {
   ColorScheme toColorScheme() {
     return ColorScheme(
-      brightness: this["brightness"],
-      background: this["background"],
-      error: this["error"],
-      onBackground: this["onBackground"],
-      onError: this["onError"],
-      onPrimary: this["onPrimary"],
-      onSecondary: this["onSecondary"],
-      onSurface: this["onSurface"],
-      primary: this["primary"],
-      primaryVariant: this["primaryVariant"],
-      secondary: this["secondary"],
-      secondaryVariant: this["secondaryVariant"],
-      surface: this["surface"],
+      brightness: Brightness.values.first,
+      background: Color(int.parse(this["background"])),
+      error: Color(int.parse(this["error"])),
+      onBackground: Color(int.parse(this["onBackground"])),
+      onError: Color(int.parse(this["onError"])),
+      onPrimary: Color(int.parse(this["onPrimary"])),
+      onSecondary: Color(int.parse(this["onSecondary"])),
+      onSurface: Color(int.parse(this["onSurface"])),
+      primary: Color(int.parse(this["primary"])),
+      primaryVariant: Color(int.parse(this["primaryVariant"])),
+      secondary: Color(int.parse(this["secondary"])),
+      secondaryVariant: Color(int.parse(this["secondaryVariant"])),
+      surface: Color(int.parse(this["surface"])),
     );
   }
 }

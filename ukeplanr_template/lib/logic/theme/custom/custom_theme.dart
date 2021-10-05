f9ea9be8ac8097fd7d97f32ffc2669a8e51d89bc
Halvor Vivelstad <halvorviv@gmail.com>
2021-10-05 14:05:08 +0200

    :scream: Make theme be a behaviorsubject
    Make theme be a behaviorsubject bc the new change in ThemeService now uses that instead of a behaviorsubject of type theme.

--- ukeplanr_template/lib/logic/theme/custom/custom_theme.dart
+++ ukeplanr_template/lib/logic/theme/custom/custom_theme.dart
@@ -5,18 +5,19 @@ import 'package:ukeplanr_template/logic/theme/custom/custom_color_scheme.dart';
 // !! Each time a new property is added to custom theme it also have to be added to
 // !! the toMap extension in extensions/themeData/asMap.dart. and extensios/map/asTheme.dart
 class CustomTheme {
-  ThemeData theme;
   String themeName;
+
+  final BehaviorSubject<ThemeData?> theme;
   late BehaviorSubject<Color> backgroundColor =
-      BehaviorSubject<Color>.seeded(theme.backgroundColor);
+      BehaviorSubject<Color>.seeded(theme.value!.backgroundColor);
 
   late BehaviorSubject<Color> primaryColor =
-      BehaviorSubject<Color>.seeded(theme.primaryColor);
+      BehaviorSubject<Color>.seeded(theme.value!.primaryColor);
 
   late BehaviorSubject<CustomColorScheme> colorScheme =
       BehaviorSubject<CustomColorScheme>.seeded(
     CustomColorScheme(
-      colorScheme: theme.colorScheme,
+      colorScheme: theme.value!.colorScheme,
     ),
   );
 

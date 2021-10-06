986044265d46f7a803dd863a7f88fd22d9030002
Halvor Vivelstad <halvorviv@gmail.com>
2021-10-06 17:23:27 +0200

    :calling: Call the firebase configure functions

--- ukeplanr_template/lib/start/configure_app.dart
+++ ukeplanr_template/lib/start/configure_app.dart
@@ -1,5 +1,7 @@
 import 'package:ukeplanr_template/start/animation/configure_animation.dart';
 import 'package:ukeplanr_template/start/dev/configure_mode.dart';
+import 'package:ukeplanr_template/start/firebase/analytics/configure_firebase_analytics.dart';
+import 'package:ukeplanr_template/start/firebase/configure_firebase_core.dart';
 import 'package:ukeplanr_template/start/localization/configure_localization.dart';
 import 'package:ukeplanr_template/start/navigation/routes/configure_routes.dart';
 import 'package:ukeplanr_template/start/widgets/configure_initialization.dart';
@@ -15,5 +17,7 @@ Future<void> configureApp() async {
   configureRoutes();
   configureAnimation();
   configureRunMode();
+  configureAnalytics();
+  await configureFirebaseCore();
   await configureThemes();
 }

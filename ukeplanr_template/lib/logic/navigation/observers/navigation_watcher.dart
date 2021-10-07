c309a8468a598f6679c8e2458e68c45284a5908d
Halvor Vivelstad <halvorviv@gmail.com>
2021-10-07 14:33:09 +0200

    :100: Register routes when a new route is navigated to

--- ukeplanr_template/lib/logic/navigation/observers/navigation_watcher.dart
+++ ukeplanr_template/lib/logic/navigation/observers/navigation_watcher.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class NavigationWatcher extends NavigatorObserver {
   // todo implement custom tracking feature for pages
 
   @override
-  void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) {}
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) =>
+      GetIt.instance.get<NavigationWatcher>().registerRoute(route.settings);
+
+  /// The [Navigator] popped `route`.
+  ///
+  /// The route immediately below that one, and thus the newly active
+  /// route, is `previousRoute`.
+  @override
+  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) =>
+      GetIt.instance.get<NavigationWatcher>().registerRoute(route.settings);
+
+  /// The [Navigator] removed `route`.
+  ///
+  /// If only one route is being removed, then the route immediately below
+  /// that one, if any, is `previousRoute`.
+  ///
+  /// If multiple routes are being removed, then the route below the
+  /// bottommost route being removed, if any, is `previousRoute`, and this
+  /// method will be called once for each removed route, from the topmost route
+  /// to the bottommost route.
+  @override
+  void didRemove(Route<dynamic> route, Route<dynamic>? previousRoute) =>
+      GetIt.instance.get<NavigationWatcher>().registerRoute(route.settings);
+
+  /// The [Navigator] replaced `oldRoute` with `newRoute`.
+  @override
+  void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) =>
+      GetIt.instance.get<NavigationWatcher>().registerRoute(newRoute!.settings);
 
   final List<Map<String?, RouteSettings>> _routeHistory =
       <Map<String?, RouteSettings>>[];

cf60239feeb59cdab83b492a0b75169d171a17f6
Halvor Vivelstad <halvorviv@gmail.com>
2021-12-15 20:39:33 +0100

    :bee: Better logging and create ID on DragController

--- lib/draggable/drag_controller.dart
+++ lib/draggable/drag_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
 import 'package:modlar/draggable/drag_data.dart';
+import 'package:modlar/draggable/drag_element.dart';
 import 'package:modlar/logic/logs/printer/log_service.dart';
 import 'package:rxdart/rxdart.dart';
 
@@ -11,13 +12,21 @@ class DragController {
 
   Stream<DragData?> get stream => _subject.stream;
 
+  final DragElement dragElement;
+
+  final int dragControllerIndex;
   // Get the latest value from the stream
   DragData? get value => _subject.value;
 
   // set widget
   void setWidget(Widget widget) {
-    GetIt.instance.get<LogsService>().logger!.log(Level.info, "Set widget");
+    GetIt.instance.get<LogsService>().logger!.log(Level.info,
+        "Changed DragController on DragELement ${dragElement.elementUID}:$dragControllerIndex to widget: $widget");
 
-    _subject.value = DragData(widget: widget, controller: this);
+    _subject.value =
+        DragData(widget: widget, controller: this, dragElement: dragElement);
   }
+
+  DragController(
+      {required this.dragElement, required this.dragControllerIndex});
 }
